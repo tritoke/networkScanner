86dce5730465d86b52d54323575d33e692119180
tritoke <tritoke@protonmail.com>
2018-11-14 20:48:24 +0000

    Continued research and added problem documentation.

---
+++ code/icmp-echo-send.py
@@ -0,0 +1,13 @@
+import socket
+
+# socket object         use IPV4 addr  allow raw socket access, set ICMP protocol        
+ping_sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+
+# this line sets the IP_HDRINCL attribute in SOL_IP to 1 allowing us to manually create IP headers.
+ping_sock.setsockopt(socket.SOL_IP, socket.IP_HDRINCL, 1)
+
+while 1:
+    recPacket, addr = s.recvfrom(1024)
+    icmp_header = recPacket[20:28]
+    msg_type, code, checksum, p_id, sequence = struct.unpack('bbHHh', icmp_header)
+    print "type: [" + str(msg_type) + "] code: [" + str(code) + "] checksum: [" + str(checksum) + "] p_id: [" + str(p_id) + "] sequence: [" + str(sequence) + "]"
