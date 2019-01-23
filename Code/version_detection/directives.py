accded5fce246830c58df8f25b67500e32a10117
= <=>
2019-01-23 22:57:21 +0000

    Finished version info parser.

--- Code/version_detection/directives.py
+++ Code/version_detection/directives.py
@@ -1,3 +1,7 @@
+import re
+from collections import defaultdict
+
+
 class Probe:
     """
     This class represents the Probe directive of the nmap-service-probes file.
@@ -9,7 +13,8 @@ class Probe:
         if protocol in {"TCP", "UDP"}:
             self.proto = protocol
         else:
-            raise ValueError(f"Probe object must have protocol TCP or UDP not {protocol}.")
+            raise ValueError(
+                f"Probe object must have protocol TCP or UDP not {protocol}.")
         self.name = probename
         self.string = probestring.split("|")[1]
 
@@ -24,7 +29,13 @@ class Probe:
 
 class Match:
 
-    version_info = None
+    version_info = defaultdict(str)
+    letter_to_name = {"p": "vendorproductname",
+                      "v": "version",
+                      "i": "info",
+                      "h": "hostname",
+                      "o": "operatingsystem",
+                      "d": "devicetype"}
 
     def __init__(self, service, pattern, pattern_options):
         self.service = service
@@ -32,11 +43,10 @@ class Match:
         self.pattern_options = pattern_options
 
     def add_version_info(version_string):
-        pass
-        # make a function which parses out all of the fields
-        # maybe use a regex to 
-
-
-
-
-
+        # this regular expression matches one character from pvihod
+        # followed by a / then it non-greedily matches at least one of
+        # any character followed by another slash
+        regex = re.compile(r"[pvihod]/.+?/")
+        fields = regex.findall(version_string)
+        for field in fields:
+            version_info[letter_to_name[field[0]]] = field[2:-1]
--- Code/version_detection/ssh_version_detect.py
+++ Code/version_detection/ssh_version_detect.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3.7
+import directives
 from os import getcwd
 from sys import path
 path.append(getcwd())
-import directives
 
 # filter out any unicode characters
-data = filter(lambda x: x<128, open("nmap-service-probes", "rb").read())
+data = filter(lambda x: x < 128, open("nmap-service-probes", "rb").read())
 # filter out all lines that start with hashtags
-lines = list(filter(lambda x: not x.startswith("#") and x != "", "".join(map(chr,data)).split("\n")))
+lines = list(filter(lambda x: not x.startswith("#") and x !=
+                    "", "".join(map(chr, data)).split("\n")))
 
 # parse the exclude directive
-directives.Probe.Exclude = range(int(lines[0][10:14]), int(lines[0][15:])+1)
+directives.Probe.Exclude = range(int(lines[0][10:14]), int(lines[0][15:]) + 1)
 
 probes = []
 
@@ -32,21 +33,23 @@ for line in lines:
         protocol = words[1]
         # make the rest of the line
         remainder = " ".join(words)
-        # this will find the index of the first "m" character 
+        # this will find the index of the first "m" character
         # in the remainder of the string
         m_pos = remainder.find("m")
         # the delimiter for the match is the next charcter after the m
-        delimiter = remainder[match_start+1]
+        delimiter = remainder[match_start + 1]
         # finds the start and end of the match
         match_start, match_end = [i for i, j in i, j in enumerate(remainder) if j == delimiter][:2]
         # splits to the string to match on out of the remainder
-        match_string = remainder[match_start:match_end+1]
-        next_space = remainder[match_end+2:].find(" ")
-        pattern_options = remainder[match_end+2:match_end+2+next_space]
+        match_string = remainder[match_start:match_end + 1]
+        next_space = remainder[match_end + 2:].find(" ")
+        pattern_options = remainder[match_end + 2:match_end + 2 + next_space]
         # creates new match object
         match = directives.Match(service, match_string)
         # add the version info to the match object
-        match.add_version_info(remainder[match_end+2+next_space:])
+        match.add_version_info(remainder[match_end + 2 + next_space:])
         current_probe.matches.append(match)
 
-    # TODO: more directives adn the add_version_info function
+
+
+# TODO: more directives 
