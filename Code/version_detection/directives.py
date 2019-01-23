9ef08c696afa61570a77af5c44e40a7ae2530826
= <=>
2019-01-23 00:03:23 +0000

    Continued work on version detection.

--- Code/version_detection/directives.py
+++ Code/version_detection/directives.py
@@ -2,13 +2,16 @@ class Probe:
     """
     This class represents the Probe directive of the nmap-service-probes file.
     """
+
+    exclude = None
+
     def __init__(self, protocol: str, probename: str, probestring: str):
         if protocol in {"TCP", "UDP"}:
             self.proto = protocol
         else:
             raise ValueError(f"Probe object must have protocol TCP or UDP not {protocol}.")
         self.name = probename
-        self.string = probestring
+        self.string = probestring.split("|")[1]
 
         self.matches = []
         self.softmatches = []
@@ -17,4 +20,23 @@ class Probe:
         self.tcpwrappems = 3000
         self.rarity = -1
         self.fallback = []
+
+
+class Match:
+
+    version_info = None
+
+    def __init__(self, service, pattern, pattern_options):
+        self.service = service
+        self.pattern = pattern
+        self.pattern_options = pattern_options
+
+    def add_version_info(version_string):
+        pass
+        # make a function which parses out all of the fields
+        # maybe use a regex to 
+
+
+
+
 
--- Code/version_detection/ssh_version_detect.py
+++ Code/version_detection/ssh_version_detect.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3.7
 from os import getcwd
-import sys
+from sys import path
 path.append(getcwd())
 import directives
 
@@ -10,6 +10,43 @@ data = filter(lambda x: x<128, open("nmap-service-probes", "rb").read())
 lines = list(filter(lambda x: not x.startswith("#") and x != "", "".join(map(chr,data)).split("\n")))
 
 # parse the exclude directive
-Exclude = range(int(lines[0][10:14]), int(lines[0][15:])+1)
+directives.Probe.Exclude = range(int(lines[0][10:14]), int(lines[0][15:])+1)
 
+probes = []
 
+for line in lines:
+    # new probe directive
+    if line.startswith("Probe"):
+        # parse line into probe protocol, name and probestring
+        proto, name, string = line.split()[1:]
+        # add the new probe to the end of the list of probes
+        probes.append(directives.Probe(proto, name, string))
+        # assign current_probe to the most recently added probe
+        current_probe = probes[-1]
+
+    # new match directive
+    elif line.startswith("match"):
+        # split the line into words
+        service = line.split(" ")
+        # the protocol will always be the first word
+        protocol = words[1]
+        # make the rest of the line
+        remainder = " ".join(words)
+        # this will find the index of the first "m" character 
+        # in the remainder of the string
+        m_pos = remainder.find("m")
+        # the delimiter for the match is the next charcter after the m
+        delimiter = remainder[match_start+1]
+        # finds the start and end of the match
+        match_start, match_end = [i for i, j in i, j in enumerate(remainder) if j == delimiter][:2]
+        # splits to the string to match on out of the remainder
+        match_string = remainder[match_start:match_end+1]
+        next_space = remainder[match_end+2:].find(" ")
+        pattern_options = remainder[match_end+2:match_end+2+next_space]
+        # creates new match object
+        match = directives.Match(service, match_string)
+        # add the version info to the match object
+        match.add_version_info(remainder[match_end+2+next_space:])
+        current_probe.matches.append(match)
+
+    # TODO: more directives adn the add_version_info function
